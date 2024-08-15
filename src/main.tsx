fcb789880f85eaae19717a3a2ff77ad4098c9b97
Martin Weiser <martin.weiser@gmail.com>
2024-08-15 18:17:11 +0200

    chore: updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import { LayoutBox } from "@e1011/es-kit";
 
 Amplify.configure(outputs);
 
+window.React = React
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>    
+    <LayoutBox width='100vw' height='100vh'>
+      <LayoutBox border='1px solid red' padding='1rem' width='100%' height='100%'>
+        <App />
+      </LayoutBox>  
+    </LayoutBox>
+  </StrictMode>
 );
