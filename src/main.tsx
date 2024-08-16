52cb180d6079351a264bf6dccf94f464b09278f8
Martin Weiser <martin.weiser@gmail.com>
2024-08-17 00:18:46 +0200

    chore: simplified

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,8 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
 import { Headline, LayoutBox } from "@e1011/es-kit";
 import '@e1011/es-kit/dist/ui/index.css';
-
-Amplify.configure(outputs);
 
 window.React = React
 
@@ -16,11 +11,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <LayoutBox width='100vw' height='calc(100vh - 0px)'>    
       <LayoutBox
         column
-        border='1px solid red' padding='1rem' width='100%' height='100%' justify="center" align="center">
-        
-        <Headline>Core Vite AWS</Headline>
-        
-        <App />        
+        padding='1rem' width='100%' height='100%' justify="center" align="center">        
+          <Headline>Core Vite AWS</Headline>
       </LayoutBox>  
     </LayoutBox>
   </StrictMode>
